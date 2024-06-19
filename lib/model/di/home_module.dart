80e195d8cb07eb3a99dbbc9fabead9349c20c5de
DESKTOP-M5TOENG\AHMED <ahmed.moh.saad.hussein.ali@gmail.com>
2024-06-19 12:37:33 +0300

    ✅ add dependency injection module

---
+++ lib/model/di/home_module.dart
@@ -0,0 +1,14 @@
+import 'package:injectable/injectable.dart';
+import 'package:slash/model/data/local/local_data_source.dart';
+import 'package:slash/model/data/local/local_data_source_impl.dart';
+import 'package:slash/model/data/repositry/home_repositry_impl.dart';
+import 'package:slash/model/domain/repositry/home_repositry.dart';
+
+@module
+abstract class HomeModule {
+  @singleton
+  HomeRepositry provideHomeRepository(HomeRepositoryImpl imp) => imp;
+
+  @singleton
+  LocalDataSource provideLocalDataSource(LocalDataSourceImpl imp) => imp;
+}
