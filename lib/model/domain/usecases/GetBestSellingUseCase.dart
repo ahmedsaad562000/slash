31580ea2e3759365403a07a84c0f2b271fd60479
DESKTOP-M5TOENG\AHMED <ahmed.moh.saad.hussein.ali@gmail.com>
2024-06-19 12:38:21 +0300

    📝 add use cases

---
+++ lib/model/domain/usecases/GetBestSellingUseCase.dart
@@ -0,0 +1,15 @@
+import 'package:injectable/injectable.dart';
+import 'package:slash/model/domain/data_state.dart';
+import 'package:slash/model/domain/models/product.dart';
+import 'package:slash/model/domain/repositry/home_repositry.dart';
+
+@injectable
+class GetBestSellingUseCase {
+  final HomeRepositry homeRepositry;
+
+  GetBestSellingUseCase(this.homeRepositry);
+
+  Future<DataState<List<Product>>> call() async {
+    return await homeRepositry.getBestSellingProducts();
+  }
+}
