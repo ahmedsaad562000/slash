e30cc00b0707b681a09959cf0ad0dd9bedabd01f
DESKTOP-M5TOENG\AHMED <ahmed.moh.saad.hussein.ali@gmail.com>
2024-06-22 09:34:05 +0300

    change theme colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:slash/view/screens/home_screen.dart';
-import 'package:slash/view_model/media_view_model.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
-    runApp(MyApp());
-  });
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: MediaViewModel()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Media Player',
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(),
-          primarySwatch: Colors.purple,
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            secondary: Colors.deepOrange, // Your accent color
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Media Player',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 248, 248, 248),
+            surfaceTintColor: Color.fromARGB(255, 248, 248, 248)),
+        primaryColorLight: const Color.fromARGB(255, 41, 41, 41),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 248, 248, 248),
+        dialogBackgroundColor: const Color.fromARGB(255, 238, 238, 238),
+        iconTheme: const IconThemeData(color: Colors.white),
+        cardColor: const Color.fromARGB(255, 41, 41, 41),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.deepOrange, // Your accent color
+        ),
+        textTheme: TextTheme(
+          displaySmall: GoogleFonts.urbanist(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: const Color.fromARGB(255, 41, 41, 41),
+          ),
+          displayMedium: GoogleFonts.urbanist(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: const Color.fromARGB(255, 41, 41, 41),
+          ),
+          displayLarge: GoogleFonts.urbanist(
+            fontSize: 18,
+            color: const Color.fromARGB(255, 41, 41, 41),
+          ),
+          labelSmall: GoogleFonts.urbanist(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: const Color.fromARGB(255, 41, 41, 41),
+          ),
+          labelMedium: GoogleFonts.urbanist(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+            color: const Color.fromARGB(255, 41, 41, 41),
+          ),
+          labelLarge: GoogleFonts.urbanist(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromARGB(255, 41, 41, 41),
           ),
         ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => HomeScreen(),
-        },
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+      },
     );
   }
 }
